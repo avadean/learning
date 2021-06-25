31259d387dc127e2c6b188d060412d64bdf93187
Adam Dean <adam.dean@chem.ox.ac.uk>
2021-06-25 03:08:25 +0100

    Moved converter and colors to data

--- data.py
+++ data.py
@@ -1,11 +1,42 @@
-from question import Question
+intToStr = {1: 'one',
+            2: 'two',
+            3: 'three',
+            4: 'four',
+            5: 'five',
+            6: 'six',
+            7: 'seven',
+            8: 'eight',
+            9: 'nine',
+            10: 'ten'
+            }
 
 
-cellQuestions = [Question('How many cells is a prokaryote?',
-                          [1]),
-                 Question('How many cells is a eukaryote?',
-                          ['many'],
-                          ['one', 'many'])
-                 ]
+class Colors:
+    default = '\033[39m'
+    black = '\033[30m'
+    red = '\033[31m'
+    green = '\033[32m'
+    yellow = '\033[33m'
+    blue = '\033[34m'
+    magenta = '\033[35m'
+    cyan = '\033[36m'
+    gray = '\033[37m'
+    orange = '\033[38;5;166m'
 
-questions = cellQuestions
+    lightRed = '\033[91m'
+    lightGreen = '\033[92m'
+    lightYellow = '\033[93m'
+    lightBlue = '\033[94m'
+    lightMagenta = '\033[95m'
+    lightCyan = '\033[96m'
+
+    reset = '\033[0m'
+    bold = '\033[1m'
+    italicized = '\033[3m'
+    underline = '\033[4m'
+    blink = '\033[5m'
+
+    question = bold + green
+    options = underline + yellow
+    hint = italicized + red
+    answer = blue
